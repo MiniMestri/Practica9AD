68bc300812d63b60dc3af367f6df9db1695160a4
MiniMestri <fonsild97@hotmail.com>
2024-02-03 07:43:08 +0100

    Operadores lógicos
    Creación de líneas de código demostrando operadores lógicos

---
+++ 001-JavaScript/03-Operadores.js
@@ -0,0 +1,33 @@
+//Operadores aritméticos
+console.log(1+3);
+console.log(3-1);
+console.log(2*3);
+console.log(4/2);
+console.log(3%2);
+
+//Operadores ded comparación
+console.log(4<3);
+console.log(4>3);
+
+console.log(4<=3);
+console.log(4>=3);
+
+console.log(4==3);
+console.log(4!=3);
+
+//Compara el valor del dato pero no el tipo de dato
+console.log(4 == "4")
+//Comprueba tanto el tipo de dato como el valor
+console.log(4 === "4")
+
+//Operadores booleanos
+console.log(3 == 3 && 4 == 4);
+console.log(3 == 3 && 4 == 4 && 5 == 5);
+
+console.log(3 == 3 && 4 == 4 && 5 == 4);
+
+console.log(3 == 3 || 4 == 4 || 5 == 5);
+
+console.log(3 == 3 || 4 == 4 || 5 == 4);
+console.log(3 == 3 || 4 == 3 || 5 == 4);
+console.log(3 == 2 || 4 == 3 || 5 == 4);
