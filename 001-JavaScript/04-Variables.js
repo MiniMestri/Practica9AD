55a72a18a7f29d8e22828d607c2ba319d64b53cb
MiniMestri <fonsild97@hotmail.com>
2024-02-03 07:47:58 +0100

    Variables
    Desarrollo de tipo de variables, operaciones abreviadas, incrementos o decrementos y encadenmiento

---
+++ 001-JavaScript/04-Variables.js
@@ -0,0 +1,38 @@
+//Variable global
+var edad = 45;
+console.log("Tu edad es de:");
+console.log(edad);
+
+//Variable local
+let edad = 45;
+edad = 46;
+console.log("Tu edad es de: "+edad+" años");
+
+//Variable constante
+const EDAD = 45;
+EDAD = 46;
+console.log("Tu edad es de: "+EDAD+" años");
+
+//Encadenamiento 
+var edad = 45;
+console.log("Tu edad es de: "+edad+" años");
+
+//Incrementos
+var edad = 45;
+console.log("Tu edad es de: "+edad+" años");
+edad++;
+console.log("Tu edad es de: "+edad+" años");
+edad--;
+console.log("Tu edad es de: "+edad+" años");
+
+//Operadores abreviados
+var edad = 45;
+console.log("Tu edad es de: "+edad+" años");
+edad += 20; 
+console.log("Tu edad es de: "+edad+" años");
+edad -= 5;
+console.log("Tu edad es de: "+edad+" años");
+edad *= 2;
+console.log("Tu edad es de: "+edad+" años");
+edad /= 5;
+console.log("Tu edad es de: "+edad+" años");
